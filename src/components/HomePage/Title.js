1077e2e35ad7bc6eab61f96edf3221aaefc0fbc7
RaulCT <raul.canul@springlabs.net>
2019-01-13 16:33:36 -0600

    HomePage/Title component created

---
+++ src/components/HomePage/Title.js
@@ -0,0 +1,15 @@
+import React, { Component, Fragment } from 'react'
+
+class Title extends Component {
+
+  render() {
+    return (
+      <Fragment>
+        <h1>ChatJS</h1>
+        <h2>Find new people!!</h2>
+      </Fragment>
+    )
+  }
+}
+
+export default Title
---
+++ src/containers/HomePage/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+import Title from '../../components/HomePage/Title'
+
+class HomePage extends Component {
+
+  render() {
+    return (
+      <div>
+        <Title />
+      </div>
+    )
+  }
+}
+
+export default HomePage
